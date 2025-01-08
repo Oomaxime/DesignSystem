8f84bd89f9912e388f1a8b3a41cbdf81482b9a06
AkaTFL <coldway3108@gmail.com>
2025-01-08 14:32:30 +0100

    fix du responsive
    Fix du responsive pour que les textes descriptifs soit associés à chaque champs, avec les espacements adaptés

--- src/pages/ConnexionForm.jsx
+++ src/pages/ConnexionForm.jsx
@@ -16,20 +16,25 @@ export default function ConnexionForm() {
     >
       <h1 className="text-3xl font-bold">Welcome Back !</h1>
       <h2>Today is a new day. It's your day. You shape it.<br />Sign in to start managing your projects.</h2>
-      <label>Email</label>
-      <Input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label>Password</label>
-      <Input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+
+      <div className=''> 
+        <label>Email</label>
+        <Input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className=''>
+        <label>Password</label>
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
       <p className="text-end"><Link to={"forgotpassword"} className="text-blue-500 hover:underline">Forgot Password</Link></p>
       <Button onClick={() => {}} active={true}>
         Submit
