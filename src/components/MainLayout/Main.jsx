88e72251ae5b0dbaa0141d6f895c4d99e054dcbc
Maxime <maxime.bidan33@gmail.com>
2025-01-08 16:28:15 +0100

    jsp

--- src/components/MainLayout/Main.jsx
+++ src/components/MainLayout/Main.jsx
@@ -1,11 +1,7 @@
 import PropTypes from 'prop-types';
 
 export default function Main({ children }) {
-  return (
-    <main>
-      {children}
-    </main>
-  )
+  return <main>{children}</main>;
 }
 
 Main.propTypes = {
