e09ec684346cc858afd4ff157c67eb80bd613daf
AkaTFL <coldway3108@gmail.com>
2025-01-08 11:55:06 +0100

    responsive form
    mise ne place du responsive pour l'affichage des forms

--- src/layouts/FormLayout.jsx
+++ src/layouts/FormLayout.jsx
@@ -3,14 +3,14 @@ import img from "../assets/images/illustre_auth_form.png";
 
 export default function FormLayout() {
   return (
-    <main className="min-h-screen grid grid-cols-2">
-      <aside className="w-full h-screen flex justify-center items-center">
+    <main className="flex flex-col-reverse md:min-h-screen md:grid md:grid-cols-2">
+      <aside className="flex justify-center items-center md:h-screen md:w-full p-6">
         <Outlet/>
       </aside>
-      <aside className="w-full h-screen p-2">
+      <aside className="h-3/10 md:w-full md:h-screen md:p-6">
         <img
           src={img}
-          className="w-full h-full object-cover rounded-xl"
+          className="w-full max-h-40 rounded-xl object-cover md:h-full md:max-h-full"
         />
       </aside>
     </main>
