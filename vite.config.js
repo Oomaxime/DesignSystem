6ee0837c00c8c4af9a6a75f103a229db1bf9a24c
Maxime <maxime.bidan33@gmail.com>
2025-01-09 10:32:10 +0100

    🐛 [build] try to fix build

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
-    },
-  },
+  base: "./",
 });
