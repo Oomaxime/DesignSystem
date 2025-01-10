63daa342d460bc1b15368d8777754b06e06b5d9d
Gontier <alexis.gontier03@gmail.com>
2025-01-10 05:42:47 +0100

    ✨[INPUT] Ajout de la visibilité du mot de passe dans le composant Input et mise à jour du formulaire utilisateur pour inclure un champ email

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,9 +1,24 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import Text from "./Text";
+import { FaEyeSlash, FaEye } from "react-icons/fa6";
 
 const defaultClasses = "input text-regular";
 
-export default function Input({ label = "", id = "input", type = "text", placeholder = "", value = "", onChange }) {
+export default function Input({
+  label = "",
+  id = "input",
+  type = "text",
+  placeholder = "",
+  value = "",
+  onChange,
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <div className="flex flex-col gap-2">
       {label && (
@@ -11,14 +26,31 @@ export default function Input({ label = "", id = "input", type = "text", placeho
           <Text type="text-button">{label}</Text>
         </label>
       )}
-      <input
-        id={id}
-        className={`${defaultClasses}`}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+
+      <div className="relative">
+        <input
+          id={id}
+          className={`${defaultClasses}`}
+          type={type === "password" && !showPassword ? "password" : "text"}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+
+        {type === "password" && (
+          <button
+            type="button"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? (
+              <FaEyeSlash className="text-gray-500" />
+            ) : (
+              <FaEye className="text-gray-500" />
+            )}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -2,76 +2,72 @@ import Form from '../components/ui/Form'
 import Input from '../components/ui/Input'
 import Button from '../components/ui/Button'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 
 export default function UserForm() {
   const [username, setUsername] = useState('')
-  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [rpassword, setRpassword] = useState('')
 
   return (
     <Form
-        onSubmit={(e) => {
-          e.preventDefault()
-          alert(`Username: ${username}, Name: ${name}, Password: ${password} , RepeatPassword: ${rpassword}`)
-        }}
+      onSubmit={(e) => {
+        e.preventDefault()
+      }}
+      h1="Edit Profil."
+      h2="Change your personal data."
+      buttonText="Save"
     >
-      <div>
-        <h1 className="text-5xl font-bold text-center">Edit Profil</h1>
-        <br/>
-        <h2 className="font-light text-xl text-center">Change your personal data</h2>
-      </div>
-      <div className='flex flex-col gap-10'>
-        <div className='flex flex-col gap-2'>
-          <div className="flex flex-col gap-1 ">
-            <label>Username</label>
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
             <Input
+              id="username"
+              label="Username"
               type="text"
               placeholder="Enter text here"
-              value={name}
+              value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
+
           <div className="flex flex-col gap-1">
-            <label>Email</label>
             <Input
-              type="text"
-              placeholder="Enter text here"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              id="email"
+              label="Email"
+              type="email"
+              placeholder="Enter text email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
         </div>
 
-        <div className='flex flex-col gap-2'>
+        <div className="flex flex-col gap-2">
           <div className="flex flex-col gap-1">
-            <label>Password</label>
             <Input
+              id="password1"
+              label="Confirm Password"
               type="password"
-              placeholder="................"
-              value={name}
+              placeholder="Enter your password"
+              value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
           <div className="flex flex-col gap-1">
-            <label>Confirm Password</label>
             <Input
+              id="password1"
+              label="Confirm Password"
               type="password"
-              placeholder="................"
-              value={name}
+              placeholder="Enter your password"
+              value={rpassword}
               onChange={(e) => setRpassword(e.target.value)}
             />
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col mt-6">
-        <Button onClick={() => {}} active={true}>
-          Save
-        </Button>
-      </div>
-    </Form>    
+    </Form>
   )
 }
