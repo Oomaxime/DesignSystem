5b7e685f28c8740814a458d26bc968abd98a9ec2
Maxime <maxime.bidan33@gmail.com>
2025-01-10 13:15:56 +0100

    ✨[STORIES] Refactor Avatar story to streamline argTypes and update default export for improved clarity and usability.

--- src/stories/components/ui/avatar.stories.jsx
+++ src/stories/components/ui/avatar.stories.jsx
@@ -4,26 +4,17 @@ export default {
   title: "Components/ui/Avatar",
   component: Avatar,
   argTypes: {
-    img: {
-      control: { type: "text" },
-      description: "The image URL of the avatar.",
-    },
-    imgWebp: {
-      control: { type: "text" },
-      description: "The WebP image URL of the avatar.",
-    },
-    pseudo: {
-      control: { type: "text" },
-      description: "The pseudo of the avatar.",
-    },
+    pseudo: { control: "text" },
+    img: { control: "text" },
+    imgWebp: { control: "text" },
   },
 };
 
 const Template = (args) => <Avatar {...args} />;
 
-export const ProfileAvatar = Template.bind({});
-ProfileAvatar.args = {
-  img: "https://via.placeholder.com/150",
-  imgWebp: "https://via.placeholder.com/150",
+export const Default = Template.bind({});
+Default.args = {
   pseudo: "John Doe",
+  img: "",
+  imgWebp: "",
 };
