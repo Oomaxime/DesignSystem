af0a5f9441e5dda5b0260809c37e4ce8c851a1a6
Maxime <maxime.bidan33@gmail.com>
2025-01-10 13:07:09 +0100

    ✨[STORIES] Enhance Avatar story with additional argTypes for img, imgWebp, and pseudo props to improve customization and documentation.

--- src/stories/components/ui/avatar.stories.jsx
+++ src/stories/components/ui/avatar.stories.jsx
@@ -3,12 +3,26 @@ import Avatar from "../../../components/ui/Avatar";
 export default {
   title: "Components/ui/Avatar",
   component: Avatar,
+  argTypes: {
+    img: {
+      control: { type: "text" },
+      description: "The image URL of the avatar.",
+    },
+    imgWebp: {
+      control: { type: "text" },
+      description: "The WebP image URL of the avatar.",
+    },
+    pseudo: {
+      control: { type: "text" },
+      description: "The pseudo of the avatar.",
+    },
+  },
 };
 
 const Template = (args) => <Avatar {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const ProfileAvatar = Template.bind({});
+ProfileAvatar.args = {
   img: "https://via.placeholder.com/150",
   imgWebp: "https://via.placeholder.com/150",
   pseudo: "John Doe",
