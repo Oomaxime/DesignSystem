8dcac845788a07e30cd1992a1a804d0ac58a4a79
Alexis G <alexis.gontier03@gmail.com>
2025-01-10 11:32:08 +0100

    ✨[COMPONENT] Ajout de la classe 'text-end' au lien dans le composant Form pour un alignement amélioré

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -41,7 +41,7 @@ export default function Form({
             {link.pretext}
             <Link
               to={link.url}
-              className="link"
+              className="link text-end"
             >
               {link.text}
             </Link>
